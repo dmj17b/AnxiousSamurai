e2f7c05539eaf1c6fa99df7a188cc8e3ef661234
DJay <djay@fsu.edu>
2024-02-04 12:39:40 -0500

    Got OSC stuff working. Still needs commenting and tweaking

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#else
+#include <WiFi.h>
+#endif
+#include <WiFiUdp.h>
+#include <OSCMessage.h>
+#include <OSCBundle.h>
+#include <OSCData.h>
 
-// How many leds in your strip?
+// Setting up the WiFi connection
+char ssid[] = "911Wifi";          // your network SSID (name)
+char pass[] = "Seminoles1!";     // your network password
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP Udp;
+const IPAddress outIp(10,40,10,105);        // remote IP (not needed for receive)
+const unsigned int outPort = 9999;          // remote port (not needed for receive)
+const unsigned int localPort = 8888;        // local port to listen for UDP packets (here's where we send the packets)
+OSCErrorCode error;
+
+
+// Number of LEDs in the strip
 #define NUM_LEDS 60
 
 // For led chips like WS2812, which have a data line, ground, and power, you just
@@ -9,27 +30,91 @@
 // ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
 // Clock pin only needed for SPI based chipsets when not using hardware SPI
 #define DATA_PIN 12
-#define CLOCK_PIN 13
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
+
+int lightState = 0;
+
+// Function prototypes
 void loadingBar(CRGB color,int speed);
 void splotchyFun(CRGB color, int speed);
 
+
+
+/**************** SETUP *****************/
 void setup() { 
     FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical
     Serial.begin(115200);
+    // Connect to WiFi network
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, pass);
+
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("");
+
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    Serial.println("Starting UDP");
+    Udp.begin(localPort);
+    Serial.print("Local port: ");
+  #ifdef ESP32
+    Serial.println(localPort);
+  #else
+    Serial.println(Udp.localPort());
+  #endif
+
 }
 
+
+/**************** LOOP *****************/
 void loop() { 
+  OSCMessage msg;
+  int size = Udp.parsePacket();
+    if (size > 0) {
+    while (size--) {
+      msg.fill(Udp.read());
+    }
+    if (!msg.hasError()) {
+      Serial.print("Received message: ");
+      lightState = msg.getBoolean(0);
+      Serial.println(msg.getBoolean(0));
+    } else {
+      error = msg.getError();
+      Serial.print("error: ");
+      Serial.println(error);
+    }
+  }
 
+  if(lightState == 1){
+    Serial.println("Light is on");
+    for(int i=0; i<NUM_LEDS; i++) {
+      leds[i] = CRGB(0,0,150);
+      FastLED.show();
+      FastLED.setBrightness(50);
+      delay(50);
+    }
+  } else if(lightState == 0){
+    for(int i=NUM_LEDS; i>=0; i--) {
+      leds[i] = CRGB::Black;
+      FastLED.show();
+      delay(50);
+    }
+  }
 
-  loadingBar(CRGB(0,0,150), 50);
-  // splotchyFun(CRGB(0,0,150), 50);
 
 }
 
 
+/*FUNCTIONS*/
 
 // Function to create a loading bar effect with LED strip
 void loadingBar(CRGB color,int speed) {
@@ -49,6 +134,7 @@ void loadingBar(CRGB color,int speed) {
 
 }
 
+// Function to create a splotchy effect with LED strip
 void splotchyFun(CRGB color, int speed){
 
   int groupIndex = 10;
